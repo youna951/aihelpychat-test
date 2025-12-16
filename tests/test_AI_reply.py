ccf1d83b6af377e349235d4b9c20aa8754004302
jeannmori <yejinb458@naver.com>
2025-12-16 14:29:18 +0900

    다시 생성 버튼 클릭

--- tests/test_AI_reply.py
+++ tests/test_AI_reply.py
@@ -57,6 +57,26 @@ class TestAIReply:
         assert pasted_text == ai_reply
         print("답변 복사 기능 테스트 완료!")
 
+#    ---------------------------
+#     답변 다시생성 기능 (AHCT-T168)
+#     AI가 준 답변에서의 하단의 '다시생성' 버튼 테스트
+#     ---------------------------
+
+    def test_reply_regenerate(self,prepare_AI_reply):
+        driver = prepare_AI_reply
+        wait = WebDriverWait(driver, 10)
+        page = ChatMainPage(driver)
+        page.input_textarea("가나디로 삼행시 지어줘")
+        page.send_button_click()
+        page.check_UI_visible("가나디로 삼행시 지어줘")
+
+
+        regenerate_button = page.get_regenerate_button()
+        page.click_regenerate_button()
+
+        time.sleep(4)
+        print("답변 다시생성 버튼 클릭 완료!")
+
 
 
 
